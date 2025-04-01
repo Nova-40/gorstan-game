be0ac470b1ebf9f9b3a2a2e2ae010e5e4cee5a56
Geoff Webster <gawebster@me.com>
2025-04-01 14:55:36 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold">Welcome to Gorstan Protocols</h1>
+      <p className="mt-4 text-gray-600">The game is loading...</p>
+    </div>
+  );
+}
+
+export default App;
